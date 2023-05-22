00021e216a25135b80de2568e20123bb51b6a217
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2023-05-23 06:00:02 +0800

    Fix favicon

--- src/root.tsx
+++ src/root.tsx
@@ -21,10 +21,10 @@ export default function Root() {
         <Title>SolidStart - Bare</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/wss/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/wss/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/wss/favicon-16x16.png" />
+				<link rel="manifest" href="/wss/site.webmanifest" />
       </Head>
       <Body>
         <Suspense>
