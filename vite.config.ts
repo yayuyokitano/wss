2f5a15918d97d9b99595897edcf5013a6251911f
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2023-05-23 05:40:09 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import solid from "solid-start/vite";
+import staticAdapter from "solid-start-static";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	base: "/wss",
+  plugins: [solid({ adapter: staticAdapter() })],
+});
