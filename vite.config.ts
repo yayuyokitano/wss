bcea977f08e6cc64889bf84014aa9b1c84309876
yayuyokitano <69117606+yayuyokitano@users.noreply.github.com>
2023-05-23 05:46:26 +0800

    add slash

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import staticAdapter from "solid-start-static";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	base: "/wss",
+	base: "/wss/",
   plugins: [solid({ adapter: staticAdapter() })],
 });
